cf5281cbfd41927e537051fea84b9cc63be0c409
unknown <igormelo9001@gmail.com>
2019-08-19 18:13:58 -0300

    features/toolbar and other configs

---
+++ app/src/main/java/com/food/projeto/solofood/activity/ConfiguracoesEmpresaActivity.java
@@ -0,0 +1,17 @@
+package com.food.projeto.solofood.activity;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.food.projeto.solofood.R;
+
+public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes_empresa);
+    }
+}
--- app/src/main/java/com/food/projeto/solofood/activity/EmpresaActivity.java
+++ app/src/main/java/com/food/projeto/solofood/activity/EmpresaActivity.java
@@ -1,17 +1,77 @@
 package com.food.projeto.solofood.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.food.projeto.solofood.R;
+import com.food.projeto.solofood.helper.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EmpresaActivity extends AppCompatActivity {
 
+    private FirebaseAuth autenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Ifood - empresa");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_empresa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+            deslogarUsuario();
+            break;
+            case R.id.menuConfiguracoes:
+            abrirConfiguracoes();
+            break;
+            case R.id.menuNovoProduto:
+            abrirNovoProduto();
+            break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void abrirNovoProduto() {
+        startActivity(new Intent(EmpresaActivity.this, NovoProdutoEmpresaActivity.class));
+    }
+
+    private void abrirConfiguracoes() {
+        startActivity(new Intent(EmpresaActivity.this, ConfiguracoesEmpresaActivity.class));
+    }
+
+
+
+    private void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
