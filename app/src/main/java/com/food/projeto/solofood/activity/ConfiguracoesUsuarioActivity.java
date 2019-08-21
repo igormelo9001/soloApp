4c0301d6bed7aca89a9486329cc26a9c1503a60f
unknown <igormelo9001@gmail.com>
2019-08-21 17:00:46 -0300

    user data recovered

--- app/src/main/java/com/food/projeto/solofood/activity/ConfiguracoesUsuarioActivity.java
+++ app/src/main/java/com/food/projeto/solofood/activity/ConfiguracoesUsuarioActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,7 +15,10 @@ import com.food.projeto.solofood.R;
 import com.food.projeto.solofood.helper.ConfiguracaoFirebase;
 import com.food.projeto.solofood.helper.UsuarioFirebase;
 import com.food.projeto.solofood.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
 
@@ -36,9 +40,31 @@ public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
         idUsuario = UsuarioFirebase.getIdUsuario();
         inicializarComponentes();
 
+        recuperarDadosUsuario();
     }
 
-    @SuppressLint("ShowToast")
+    private void recuperarDadosUsuario(){
+
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios")
+                .child(idUsuario);
+
+        usuarioRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null){
+                    Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                    editUsuarioNome.setText(usuario.getNome());
+                    editUsuarioEndereco.setText(usuario.getEndereco());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     private void exibirMensagem(String texto){
         Toast.makeText(ConfiguracoesUsuarioActivity.this, texto, Toast.LENGTH_LONG).show();
     }
