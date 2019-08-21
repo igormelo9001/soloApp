6dcdbf2be2c15c41554cbe2eaea3ea3e6f19438d
unknown <igormelo9001@gmail.com>
2019-08-21 16:54:08 -0300

    feature/ saving user data

--- app/src/main/java/com/food/projeto/solofood/activity/ConfiguracoesUsuarioActivity.java
+++ app/src/main/java/com/food/projeto/solofood/activity/ConfiguracoesUsuarioActivity.java
@@ -1,13 +1,26 @@
 package com.food.projeto.solofood.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.food.projeto.solofood.R;
+import com.food.projeto.solofood.helper.ConfiguracaoFirebase;
+import com.food.projeto.solofood.helper.UsuarioFirebase;
+import com.food.projeto.solofood.model.Usuario;
+import com.google.firebase.database.DatabaseReference;
 
 public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
+
+    private EditText editUsuarioNome, editUsuarioEndereco;
+    private String idUsuario;
+    private DatabaseReference firebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +31,47 @@ public class ConfiguracoesUsuarioActivity extends AppCompatActivity {
         toolbar.setTitle("Configurações usuário");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuario = UsuarioFirebase.getIdUsuario();
+        inicializarComponentes();
+
+    }
+
+    @SuppressLint("ShowToast")
+    private void exibirMensagem(String texto){
+        Toast.makeText(ConfiguracoesUsuarioActivity.this, texto, Toast.LENGTH_LONG).show();
+    }
+
+    public void validarDadosUsuario(View view){
+
+        String nome = editUsuarioNome.getText().toString();
+        String endereco = editUsuarioEndereco.getText().toString();
+
+        if(!nome.isEmpty()){
+            if(!endereco.isEmpty()){
+
+                Usuario usuario = new Usuario();
+                usuario.setIdUsuario(idUsuario);
+                usuario.setNome(nome);
+                usuario.setEndereco(endereco);
+                usuario.salvar();
+
+                exibirMensagem("Dados atualizados com sucesso");
+                finish();
+
+            }else {
+                exibirMensagem("Digite seu endereço completo!");
+            }
+        }else {
+            exibirMensagem("Digite seu nome");
+        }
+
+    }
+
+    private void inicializarComponentes(){
+        editUsuarioNome = findViewById(R.id.editUsuarioNome);
+        editUsuarioEndereco = findViewById(R.id.editUsuarioEndereco);
+
     }
 }
