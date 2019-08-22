500d81ab6edba4087da7f7a078f7d5031ad0d55a
unknown <igormelo9001@gmail.com>
2019-08-22 08:41:25 -0300

    feature/ add order food

--- app/src/main/java/com/food/projeto/solofood/activity/CardapioActivity.java
+++ app/src/main/java/com/food/projeto/solofood/activity/CardapioActivity.java
@@ -1,6 +1,10 @@
 package com.food.projeto.solofood.activity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,8 +17,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.food.projeto.solofood.R;
 import com.food.projeto.solofood.adapter.AdapterProduto;
 import com.food.projeto.solofood.helper.ConfiguracaoFirebase;
+import com.food.projeto.solofood.helper.UsuarioFirebase;
 import com.food.projeto.solofood.model.Empresa;
 import com.food.projeto.solofood.model.Produto;
+import com.food.projeto.solofood.model.Usuario;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +29,8 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class CardapioActivity extends AppCompatActivity {
 
@@ -34,6 +42,9 @@ public class CardapioActivity extends AppCompatActivity {
     private List<Produto> produtos = new ArrayList<>();
     private DatabaseReference firebaseRef;
     private String idEmpresa;
+    private AlertDialog dialog;
+    private String idUsuarioLogado;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,8 @@ public class CardapioActivity extends AppCompatActivity {
 
         inicializarComponentes();
         firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
+
 
         Bundle bundle = getIntent().getExtras();
 
@@ -67,6 +80,42 @@ public class CardapioActivity extends AppCompatActivity {
         recyclerProdutosCardapio.setAdapter(adapterProduto);
 
         recuperarProdutos();
+        recuperarDadosUsuario();
+    }
+
+    private void recuperarDadosUsuario(){
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando dados")
+                .setCancelable(false)
+                .build();
+        dialog.show();
+
+        DatabaseReference usuariosRef = firebaseRef
+                .child("usuarios")
+                .child(idUsuarioLogado);
+
+        usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null){
+                    usuario = dataSnapshot.getValue(Usuario.class);
+                }
+                recuperarPedido();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void recuperarPedido() {
+
+        dialog.dismiss();
 
     }
 
@@ -102,5 +151,24 @@ public class CardapioActivity extends AppCompatActivity {
         recyclerProdutosCardapio = findViewById(R.id.recyclerProdutosCardapio);
         imageEmpresaCardapio = findViewById(R.id.imageEmpresaCardapio);
         textNomeEmpresaCardapio = findViewById(R.id.textNomeEmpresaCardapio);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cardapio, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuPedido:
+                
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
